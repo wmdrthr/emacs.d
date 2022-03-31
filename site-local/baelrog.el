a256f81e1e49f761c3081108839814414e30c9ae
Shantanu Joshi <weemadarthur@yggdrasil.in>
2022-03-31 11:24:48 +0530

    Add site local customizations file for baelrog

---
+++ site-local/baelrog.el
@@ -0,0 +1,14 @@
+;; -*- mode: Emacs-Lisp -*-
+;; local.el
+;; Time-stamp: <2021-05-05 16:02:51 shantanu>
+
+(setq user-mail-address "shantanu@helpshift.com")
+
+(setq w/features
+      (append w/features '(fira-code
+                           ligatures
+                           org-mode
+                           rust
+                           clojure)))
+
+;(push 'fira-code-ligatures packages)
